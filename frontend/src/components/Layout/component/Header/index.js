1d5f5ae0d5ff786e2fb4a8e387b18cbb074d2fd8
HoangMaiLapTrinh <hoangmai020603@gmail.com>
2025-04-17 15:59:09 +0700

    fix scss and update header
    header basic

--- frontend/src/components/Layout/component/Header/index.js
+++ frontend/src/components/Layout/component/Header/index.js
@@ -1,19 +1,54 @@
 import classNames from 'classnames/bind';
-import styles from './Header.module.scss';
+import * as styles from './Header.module.scss';
 import React from 'react';
-import logo_rmbg from 'D:/Program Files/GitHub/BTL_WEB/frontend/src/img/logo-rmbg.png';
+import logo_rmbg from '../../../../img/logo-rmbg.png';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import { Link } from 'react-router-dom';
+
 const cx = classNames.bind(styles);
-console.log(styles);
 
 function Header() {
-    return <header className={cx('wrapper')}>
-        <div className={cx('logo')}>
-            <img src={logo_rmbg} alt="Logo" className={cx('logo_rmbg')} />
-        </div>
-        <div className={cx('inner')}>
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('nav')}>
+                <div className={cx('nav-left')}>
+                    <ul className={cx('main-menu')}>
+                        <li className={cx('nav-item')}>
+                            <Link to="/products" className={cx('nav-link')}>SẢN PHẨM</Link>
+                        </li>
+                        
+                        <li className={cx('nav-item', 'dropdown')}>
+                            <span className={cx('nav-link', 'dropdown-toggle')}>
+                                THƯƠNG HIỆU
+                            </span>
+                            <div className={cx('dropdown-menu')}>
+                                <Link className={cx('dropdown-item')} to="/product?brand=casio">Casio</Link>
+                                <Link className={cx('dropdown-item')} to="/product?brand=pierre-nannier">Pierre nannier</Link>
+                                <Link className={cx('dropdown-item')} to="/product?brand=tudor">Tudor</Link>
+                                <Link className={cx('dropdown-item')} to="/product?brand=tissot">Tissot</Link>
+                                <Link className={cx('dropdown-item')} to="/product?brand=g-shock">G-shock</Link>
+                                <Link className={cx('dropdown-item')} to="/product?brand=orient">Orient</Link>
+                                <Link className={cx('dropdown-item')} to="/product?brand=citizen">Citizen</Link>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
 
-        </div>
-    </header>
+                <div className={cx('logo')}>
+                    <Link to="/"><img src={logo_rmbg} alt="Logo" className={cx('logo_rmbg')} /></Link>
+                </div>
+
+                <div className={cx('nav-right')}>
+                    <ul className={cx('main-menu')}>
+                        <li className={cx('nav-item')}>
+                            <Link to="/cart" className={cx('nav-link')}>GIỎ HÀNG</Link>
+                        </li>
+                    </ul>
+                        
+                </div>
+            </div>
+        </header>
+    );
 }
 
 export default Header;
