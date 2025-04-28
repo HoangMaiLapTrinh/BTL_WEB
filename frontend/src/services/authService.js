8dc1290826a8c8abf1b16e111c37571f34d6f149
HoangMaiLapTrinh <hoangmai020603@gmail.com>
2025-04-28 22:44:03 +0700

    test 2
    test 2

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = 'http://localhost:5000/api';
+export const API_URL = 'https://btl-web-avdc.onrender.com/api';
 
 // Cấu hình axios mặc định
 axios.defaults.withCredentials = true;
