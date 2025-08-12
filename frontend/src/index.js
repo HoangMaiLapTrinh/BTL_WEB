e975c659867a55c017ebcbe37ccba207847f3201
HoangMaiLapTrinh <hoangmai020603@gmail.com>
2025-08-12 12:18:27 +0700

    test 24
    fix name app

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store.js';
-import App from './App.js';
+import App from './app.js';
 import GlobalStyles from './components/GlobalStyles/index.js';
 import './components/GlobalStyles/GlobalStyles.scss';
 const root = ReactDOM.createRoot(document.getElementById('root'));
